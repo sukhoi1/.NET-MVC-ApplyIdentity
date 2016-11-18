2be3b82e195e07a27b8b085d2344357a2e0984f6
Anton Lygin <lygin@live.com>
2016-11-18 14:53:52 +0300

    v0.001 - initial IdentityStart commit;

---
+++ IdentityStart/Infrastructure/AppIdentityDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ApplyIdentity.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ApplyIdentity.Infrastructure
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext() : base("IdentityDb")
+        {
+        }
+
+        static AppIdentityDbContext()
+        {
+            System.Data.Entity.Database.SetInitializer<AppIdentityDbContext>(new IdentityDbInit());
+        }
+
+        public static AppIdentityDbContext Create()
+        {
+            return new AppIdentityDbContext();
+        }
+    }
+}
---
+++ IdentityStart/Models/CreateModel.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation.Attributes;
+using ApplyIdentity.Infrastructure;
+
+namespace ApplyIdentity.Models
+{
+    [Validator(typeof(FluentCreateModelValidator))]
+    public class CreateModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
